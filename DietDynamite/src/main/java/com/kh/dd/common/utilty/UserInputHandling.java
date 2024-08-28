491fd32f64c575396211ef0312ec32bfd5d4f5cb
최명식 <audtlr93@naver.com>
2024-08-28 16:40:26 +0900

    [Structure] 불필요 코드 제거

--- DietDynamite/src/main/java/com/kh/dd/common/utilty/UserInputHandling.java
+++ DietDynamite/src/main/java/com/kh/dd/common/utilty/UserInputHandling.java
@@ -1,5 +1,5 @@
 package com.kh.dd.common.utilty;
 
-public class FileUtil {
+public class UserInputHandling {
 
 }
--- DietDynamite/src/main/java/com/kh/dd/controller/LoginController.java
+++ DietDynamite/src/main/java/com/kh/dd/controller/LoginController.java
@@ -1,15 +1,18 @@
 package com.kh.dd.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/login")
+@PropertySource("classpath:/app.properties")
 public class LoginController {
 	
-	@GetMapping("")
-	public String login() {
-		return "login/login";
-	}
+    // app.properties 내의 KAKAO_APP_KEY 값을 주입받음
+    @Value("${app.KAKAO_APP_KEY}")
+    private String KAKAO_APP_KEY;
 }
